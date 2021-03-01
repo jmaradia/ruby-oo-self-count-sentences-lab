7af5422476872a5dcff20230f7e896e39f62c1df
josemaradiaga <josemarad25@gmail.com>
2021-03-01 09:40:22 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,40 @@
 require 'pry'
 
-class String
 
+class String
+# ### The `#sentence?` Method
+
+# Define this instance method to return `true` if the string you are calling it on ends in a period and false if it does not. 
+
+# You will need to use the `self` keyword in the body of this method to refer to the string on which the method is being called. You can use the `#end_with?` method to determine what the final character in a string is. 
+
+# For example:
+
+# ```ruby
+# "HI!".end_with?("!")
+#   => true
+# ```
+
+# Call the `end_with?` method on `self` inside of your `#sentence?` method. 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    #binding.pry
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?('!')
 
   end
 
   def count_sentences
+    self.split(/[.?!]+/).count
 
   end
 end
+
+##test 
